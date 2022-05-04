e7f264162de120bc4bee91203fdbfb2a10898c26
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-05-04 10:21:22 -0700

    import math n=int(input()) temp=n c=1 sq=n*n temp=n while(n>0):     c=c*10     n=n//10 if(sq%c==temp):     print("Automorphic Number") else:     print("Not an Automorphic Number");

---
+++ Automorphic_Number.py
@@ -0,0 +1,13 @@
+import math
+n=int(input())
+temp=n
+c=1
+sq=n*n
+temp=n
+while(n>0):
+    c=c*10
+    n=n//10
+if(sq%c==temp):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number");
