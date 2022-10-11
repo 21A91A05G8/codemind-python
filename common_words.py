e13492f8d9f8999b3dacda543f40a25724db7893
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-10-11 15:09:45 +0530

    s1=input().lower().split() s2=input().lower().split() #a=list(s1.split(" ")) #b=list(s2.split(" ")) c=[] for i in s2:     if i in s1 and i not in c:         c.append(i) print(" ".join(c))

--- common_words.py
+++ common_words.py
@@ -1,11 +1,9 @@
-s1=input()
-s2=input()
-s1=s1.lower()
-s2=s2.lower()
-a=list(s1.split(" "))
-b=list(s2.split(" "))
+s1=input().lower().split()
+s2=input().lower().split()
+#a=list(s1.split(" "))
+#b=list(s2.split(" "))
 c=[]
-for i in b:
-    if i in a and i not in c:
+for i in s2:
+    if i in s1 and i not in c:
         c.append(i)
 print(" ".join(c))        
