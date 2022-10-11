cf7c3187811bf71b385373070a3413357b55ae76
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-10-11 14:52:17 +0530

    s1=input().lower() s2=input().lower() k=[] for i in s1:     if i not in s2:         k.append(i) for i in s2:     if i not in s1:         k.append(i) for i in sorted(set(k)):     if i!=' ':         print(i,end='')

---
+++ non_common_characters.py
@@ -0,0 +1,13 @@
+s1=input().lower()
+s2=input().lower()
+k=[]
+for i in s1:
+    if i not in s2:
+        k.append(i)
+for i in s2:
+    if i not in s1:
+        k.append(i)
+for i in sorted(set(k)):
+    if i!=' ':
+        print(i,end='')
+        
