b3b64b11a4c26f29affb8505e40526f38f80e618
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-10-11 15:30:54 +0530

    s=input() c=0 for i in sorted(set(s)):     if(i>='a' and i<='z'):         print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,7 +1,5 @@
 s=input()
 c=0
 for i in sorted(set(s)):
-    if i>="a" and i<="z":
-        if s.count(i)==1:
-            print(i,end="")
-    
+    if(i>='a' and i<='z'):
+        print(i,end='')
